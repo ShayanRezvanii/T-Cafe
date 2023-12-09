8646a660cd6a95868b65ce810698501d1df8dc06
Shayan Rezvani <84992337+ShayanRezvanii@users.noreply.github.com>
2023-12-09 14:37:32 +0330

    First

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-full w-full max-w-[480px] bg-black'></div>
+    </>
+  )
+}
+
+export default App
