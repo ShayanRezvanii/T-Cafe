f481bc345695687e25bf726e273ec792145b5aaf
Shayan Rezvani <84992337+ShayanRezvanii@users.noreply.github.com>
2023-12-25 10:38:09 +0330

    Update !

--- my-react-app/src/Components/Desktop/ProductsDesktop.jsx
+++ my-react-app/src/Components/Desktop/ProductsDesktop.jsx
@@ -1,28 +1,20 @@
-import React from 'react'
-import Product from '../Product'
+import React from 'react';
+import Product from '../Product';
+import data from '../../../Data/products.json';
 
-function ProductsDesktop() {
+function ProductsDesktop({selectedValue}) {
+  const filtered = data.categories.filter((item) => item.name === selectedValue);
+
+  const filteredItems = filtered.length > 0 ? filtered[0].items : [];
+  console.log(filteredItems);
+
   return (
-    <div className=' w-full h-fit gap-4 p-10 overflow-y-scroll flex flex-wrap  '>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-   
-
-        
-
+    <div className='w-full h-fit gap-6 items-center justify-between p-10 flex flex-wrap'>
+      {filteredItems.map((item) => (
+        <Product key={item.id} {...item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductsDesktop
+export default ProductsDesktop;
--- my-react-app/src/Components/Desktop/Slider.jsx
+++ my-react-app/src/Components/Desktop/Slider.jsx
@@ -1,62 +1,167 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SwiperSlide, Swiper, useSwiper } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import coffee from "/Products/Coffee-Beans.jpg";
 import NextButton from "./NextButton";
-import Products from "../Products";
-import Product from "../Product";
-import data from '../../../Data/products.json'
+import pizza from "/Products/pizza.png";
 import ProductsDesktop from "./ProductsDesktop";
+import Logo from "../../../public/Background/tcafe.png";
+import coffeeProduct from '../../../public/Products/coffe.jpg'
+import cake from '../../../public/Products/cake-16.png'
+import limo from '../../../public/Products/limo.png'
+import {
+  MouseParallaxContainer,
+  MouseParallaxChild,
+} from "react-parallax-mouse";
 
 function Slider() {
-  const swiper = useSwiper()
-
 
   return (
-    <>
       <Swiper
-        className=" w-full h-screen "
+        className=" w-full h-screen  "
         direction={"vertical"}
-        allowTouchMove={false}
+        allowTouchMove={true}
         pagination={{
           clickable: true,
+          
         }}
-
         modules={[Pagination]}
       >
         <SwiperSlide>
           <img src={coffee} className=" w-full h-full" />
           <div className=" bg-black flex flex-col h-screen top-0 bg-black/30  absolute w-full">
             <div className=" w-full h-20 py-16 px-20">
-              <h1 className=" text-[140px] text-right text-yellow-900">تی کافه</h1>
+              <h1 className=" text-[140px] text-right text-yellow-900">
+                تی کافه
+              </h1>
               <h3 className=" text-right text-5xl">خوش آمدید</h3>
             </div>
             <div className=" w-full flex justify-center h-screen items-end">
               <NextButton>مشاهده منو</NextButton>
             </div>
           </div>
-
-         </SwiperSlide>
+        </SwiperSlide>
 
         <SwiperSlide>
-            <div className=' w-full h-full flex'>
-        <div className=' flex flex-col  w-full h-full'>
-            <div className=' text-2xl bg-yellow-300 w-full flex justify-center items-center max-h-[170px] h-full'>
-              LOGO
+          <div className=" w-full h-full flex">
+            <div className=" flex flex-col bg-black  w-full h-full">
+              <div className=" text-2xl  w-full flex justify-center items-center h-[170px]">
+                <div>
+                  <img
+                    src={Logo}
+                    width={230}
+                    height={230}
+                    className=" object-cover "
+                  />
+                </div>
+              </div>
+              <div className="  w-full scrollbar-thin scrollbar-thumb-rounded-full overflow-y-scroll flex-1  h-full">
+                <ProductsDesktop selectedValue={3} />
+              </div>
             </div>
-            <div className=' bg-red-300 w-full  h-full'>
-            <ProductsDesktop />
-                
+            <MouseParallaxContainer
+              globalFactorX={0.1}
+              globalFactorY={0.1}
+              className=" w-full flex object-cover justify-center items-center bg-white h-full"
+            >
+              <MouseParallaxChild factorX={0.3} factorY={0.5}>
+                <img src={coffeeProduct} className=" object-cover w-[600px] h-[600px]" />
+              </MouseParallaxChild>
+            </MouseParallaxContainer>
+          </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+          <div className=" w-full h-full flex">
+            <div className=" flex flex-col bg-black  w-full h-full">
+              <div className=" text-2xl  w-full flex justify-center items-center h-[170px]">
+                <div>
+                  <img
+                    src={Logo}
+                    width={230}
+                    height={230}
+                    className=" object-cover "
+                  />
+                </div>
+              </div>
+              <div className="  w-full scrollbar-thin scrollbar-thumb-rounded-full overflow-y-scroll flex-1  h-full">
+                <ProductsDesktop selectedValue={1} />
+              </div>
             </div>
+            <MouseParallaxContainer
+              globalFactorX={0.1}
+              globalFactorY={0.1}
+              className=" w-full flex object-cover justify-center items-center bg-white h-full"
+            >
+              <MouseParallaxChild factorX={0.3} factorY={0.5}>
+                <img src={pizza} className=" object-cover w-[600px] h-[600px]" />
+              </MouseParallaxChild>
+            </MouseParallaxContainer>
+          </div>
+        </SwiperSlide>
 
-        </div>
-        <div className=' bg-green-300 w-full h-full'></div>
+        
 
-    </div>
-        </SwiperSlide>  
+        <SwiperSlide>
+          <div className=" w-full h-full flex">
+            <div className=" flex flex-col bg-black  w-full h-full">
+              <div className=" text-2xl  w-full flex justify-center items-center h-[170px]">
+                <div>
+                  <img
+                    src={Logo}
+                    width={230}
+                    height={230}
+                    className=" object-cover "
+                  />
+                </div>
+              </div>
+              <div className="  w-full scrollbar-thin scrollbar-thumb-rounded-full overflow-y-scroll flex-1  h-full">
+                <ProductsDesktop selectedValue={0} />
+              </div>
+            </div>
+            <MouseParallaxContainer
+              globalFactorX={0.1}
+              globalFactorY={0.1}
+              className=" w-full flex object-cover justify-center items-center bg-white h-full"
+            >
+              <MouseParallaxChild factorX={0.3} factorY={0.5}>
+                <img src={cake} className=" object-cover w-[600px] h-[600px]" />
+              </MouseParallaxChild>
+            </MouseParallaxContainer>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className=" w-full h-full flex">
+            <div className=" flex flex-col bg-black  w-full h-full">
+              <div className=" text-2xl  w-full flex justify-center items-center h-[170px]">
+                <div>
+                  <img
+                    src={Logo}
+                    width={230}
+                    height={230}
+                    className=" object-cover "
+                  />
+                </div>
+              </div>
+              <div className="  w-full scrollbar-thin scrollbar-thumb-rounded-full overflow-y-scroll flex-1  h-full">
+                <ProductsDesktop selectedValue={2} />
+              </div>
+            </div>
+            <MouseParallaxContainer
+              globalFactorX={0.1}
+              globalFactorY={0.1}
+              className=" w-full flex object-cover justify-center items-center bg-white h-full"
+            >
+              <MouseParallaxChild factorX={0.3} factorY={0.5}>
+                <img src={limo} className=" object-cover w-[600px] h-[600px]" />
+              </MouseParallaxChild>
+            </MouseParallaxContainer>
+          </div>
+        </SwiperSlide>
 
       </Swiper>
-    </>
   );
 }
 
